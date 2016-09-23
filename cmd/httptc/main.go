c4e798da6236927556ba3953f235886f61e06ff9
Aliaksandr Valialkin <valyala@gmail.com>
2016-09-23 17:58:19 +0300

    Initial implementation of httpteleport client (httptc) and server (httpts) services

---
+++ cmd/httptc/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/httpteleport"
+	"github.com/valyala/tcplisten"
+	"log"
+	"time"
+)
+
+var (
+	reusePort = flag.Bool("reusePort", false, "Whether to enable SO_REUSEPORT on listenAddr")
+
+	batchDelay  = flag.Duration("batchDelay", time.Millisecond, "How long to wait before flushing incoming requests to httpts")
+	concurrency = flag.Int("concurrency", 100000, "The maximum number of concurrent incoming connections the client may handle")
+	listenAddr  = flag.String("listenAddr", ":8042", "TCP address to listen to for incoming HTTP requests")
+	serverAddr  = flag.String("serverAddr", "127.0.0.1:8043", "TCP address of httpts server to route incoming requests to")
+	timeout     = flag.Duration("timeout", time.Second, "Maximum duration for waiting response from httpteleport server")
+)
+
+func main() {
+	flag.Parse()
+
+	c = httpteleport.Client{
+		Addr:               *serverAddr,
+		MaxBatchDelay:      *batchDelay,
+		MaxPendingRequests: *concurrency,
+	}
+
+	cfg := tcplisten.Config{
+		ReusePort: *reusePort,
+	}
+	ln, err := cfg.NewListener("tcp4", *listenAddr)
+	if err != nil {
+		log.Fatalf("cannot listen to -listenAddr=%q: %s", *listenAddr, err)
+	}
+
+	s := fasthttp.Server{
+		Handler:     requestHandler,
+		Concurrency: *concurrency,
+	}
+
+	log.Printf("listening for HTTP requests on %q", *listenAddr)
+	log.Printf("forwarding requests to httpts %q", *serverAddr)
+
+	if err := s.Serve(ln); err != nil {
+		log.Fatalf("error in fasthttp server: %s", err)
+	}
+}
+
+var c httpteleport.Client
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	err := c.DoTimeout(&ctx.Request, &ctx.Response, *timeout)
+	if err == nil {
+		return
+	}
+
+	ctx.ResetBody()
+	fmt.Fprintf(ctx, "httptc proxying error: %s", err)
+	if err == httpteleport.ErrTimeout {
+		ctx.SetStatusCode(fasthttp.StatusGatewayTimeout)
+	} else {
+		ctx.SetStatusCode(fasthttp.StatusBadGateway)
+	}
+}
---
+++ cmd/httpts/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/httpteleport"
+	"github.com/valyala/tcplisten"
+	"log"
+	"strings"
+	"time"
+)
+
+var (
+	batchDelay  = flag.Duration("batchDelay", time.Millisecond, "How long to wait before flushing responses back to httptc")
+	concurrency = flag.Int("concurrency", 100000, "The maximum number of concurrent requests the server may handle")
+	listenAddr  = flag.String("listenAddr", ":8043", "TCP address to listen to for httptc connections")
+	serverAddr  = flag.String("serverAddr", "127.0.0.1:8044", "Comma-separated list of upstream HTTP server TCP addresses "+
+		"to forward requests to.\n"+
+		"Each request is forwared to the least loaded upstream server")
+	timeout = flag.Duration("timeout", time.Second, "Maximum duration for waiting response from upstream HTTP servers")
+)
+
+func main() {
+	flag.Parse()
+
+	for _, addr := range strings.Split(*serverAddr, ",") {
+		c := &fasthttp.HostClient{
+			Addr: addr,
+		}
+		cs = append(cs, c)
+	}
+
+	cfg := tcplisten.Config{}
+	ln, err := cfg.NewListener("tcp4", *listenAddr)
+	if err != nil {
+		log.Fatalf("cannot listen to -listenAddr=%q: %s", *listenAddr, err)
+	}
+
+	s := httpteleport.Server{
+		Handler:       requestHandler,
+		Concurrency:   *concurrency,
+		MaxBatchDelay: *batchDelay,
+	}
+
+	log.Printf("listening for httptc connections on %q", *listenAddr)
+	log.Printf("forwarding requests to upstream HTTP servers %q", *serverAddr)
+
+	if err := s.Serve(ln); err != nil {
+		log.Fatalf("error in fasthttp server: %s", err)
+	}
+}
+
+var cs []*fasthttp.HostClient
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	c := leastLoadedClient()
+	err := c.DoTimeout(&ctx.Request, &ctx.Response, *timeout)
+	if err == nil {
+		return
+	}
+
+	ctx.ResetBody()
+	fmt.Fprintf(ctx, "httpts proxying error: %s", err)
+	if err == fasthttp.ErrTimeout {
+		ctx.SetStatusCode(fasthttp.StatusGatewayTimeout)
+	} else {
+		ctx.SetStatusCode(fasthttp.StatusBadGateway)
+	}
+}
+
+func leastLoadedClient() *fasthttp.HostClient {
+	minC := cs[0]
+	minN := minC.PendingRequests()
+	if minN == 0 {
+		return minC
+	}
+	for _, c := range cs[1:] {
+		n := c.PendingRequests()
+		if n == 0 {
+			return c
+		}
+		if n < minN {
+			minC = c
+			minN = n
+		}
+	}
+	return minC
+}
